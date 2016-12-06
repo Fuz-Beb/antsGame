80a28a12acc66691771e4a18ae70b4b5738f1501
RBouteloup <remy.bouteloup@etu.univ-tours.fr>
2016-12-06 14:43:25 +0100

    Exercice 1 OK

---
+++ tp1_a.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    int i = 0;
+    
+    while (argc > 0)
+    {
+        printf("<%s>", argv[i]);
+        argc--;
+        i++;
+    }
+}
