01b43a5c16d631562efdf4536993ee6913a78f0f
Pierrick BOBET <pierrick.bobet@cryptolab.net>
2016-12-06 20:26:35 +0100

    Update Documentation

--- tp1_a.c
+++ tp1_a.c
@@ -1,10 +1,9 @@
 /**
- * \fn int ReadParamFromCommandLine(TParamDef * _tabParam, const int _nbParam, const int _argc, const char * _argv[])
+ * \fn int main(int argc, char * argv[])
  * \brief fonction qui affiche les paramètres qu'ils reçoit sur la ligne de commande
  *
  * \param[in] _argc nombre d'arguments passes sur la ligne de commande
  * \param[in] _argv tableau qui contient les chaines de caracteres passees en arguments du programme
- * \return >=0 : nombre de parametres mis a jour, <0 : erreur
 */
 #include <stdio.h>
 
